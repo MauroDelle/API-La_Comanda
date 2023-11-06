d5ab87172be8600ba40e91fe80987883d1f06487
Mauro <maurodelle7@gmail.com>
2023-11-06 19:11:05 -0300

    .

--- app/models/EstadoMesa.php
+++ app/models/EstadoMesa.php
@@ -5,12 +5,9 @@ class Estado
     const COMIENDO = "Comiendo";
     const PAGANDO = "Pagando";
     const CERRADA = "Cerrada";
-
     const BAJA = "Baja";
-
     const PENDIENTE = 'Pendiente';    
     const PAGADO = 'Pagado';
-
     const PREPARACION = 'En Preparacion';
     const LISTO = 'Listo para servir';
     const ENTREGADO = 'Entregado';
